3f7eb44c871162391245358b0a144a4e3f96111f
장보현 <bvcxz0525@gmail.com>
2020-12-14 18:35:50 +0000

    Update README.md, data_files/title_voca_okt.csv, voca_dtm/dtm/dtm.ipynb, voca_dtm/dtm/dtm.py files

--- voca_dtm/dtm/dtm.py
+++ voca_dtm/dtm/dtm.py
@@ -1,13 +1,13 @@
 from math import log 
 
-N = len(news_20200801)
+N = len(cnews_20200801)
 
 def tf(token, doc):
     return doc.count(token)
 
 def idf(token):
     df = 0
-    for doc in list(news_20200801['내용']):
+    for doc in list(cnews_20200801['내용']):
         df += token in doc
     return log(N / (df + 1))
 
@@ -17,7 +17,7 @@ def tf_idf(token, doc):
 tf_result = []
 for i in range(N):
     tf_result.append([])
-    doc = news_20200801['내용'][i]
+    doc = cnews_20200801['내용'][i]
     for j in range(len(VOCA_20200801)):
         token = VOCA_20200801[j]
         tf_result[-1].append(tf(token,doc))
@@ -35,7 +35,7 @@ idf_ = pd.DataFrame(idf_result,index = VOCA_20200801, columns = ['IDF'])
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-tfidf_20200801 = TfidfVectorizer(min_df=2,sublinear_tf=True).fit(news_20200801['내용'])
-print(tfidf_20200801.transform(news_20200801['내용']).toarray())
+tfidf_20200801 = TfidfVectorizer(min_df=2,sublinear_tf=True).fit(cnews_20200801['내용'])
+print(tfidf_20200801.transform(cnews_20200801['내용']).toarray())
 print(tfidf_20200801.vocabulary_)
 
